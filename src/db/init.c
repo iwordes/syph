bc6024dde3f1831baff2a55f870c1187b3de3df9
Ian Wordes <iwordes@student.42.us.org>
2017-04-22 14:45:23 -0700

    Add db_init

--- src/db/init.c
+++ src/db/init.c
@@ -6,34 +6,31 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 20:44:16 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/22 12:15:24 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/22 14:43:04 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <syph.h>
 
 #ifdef __BIG_ENDIAN__
-# define EBYTE 0xff
+# define HEAD "\xff\x2a""\0\1""\0\0\0\1""\0\0\0\1"
 #else
-# define EBYTE 0x00
+# define HEAD "\x00\x2a""\1\0""\1\0\0\0""\1\0\0\0"
 #endif
 
-static void	init_header_(int fd)
+int		db_init(const char *path)
 {
-	char	buff[4];
-
-	buff[0] = EBYTE;
-	buff[1] = 0x2a;
-	*((uint16_t*)buff + 1) = 1;
-	write(fd, buff, 4);
-	*(uint32_t*)buff = 1;
-	write(fd, buff, 4);
-	write(fd, buff, 4);
-}
-
-int		db_init(/* ... */)
-{
-	if (ftruncate(/* ... */, 16 * 1024 * 1024) < 0)
+	if ((fd = open(path, O_WRONLY, O_CREAT | O_EXCL | O_EXLOCK)) < 0)
 		return (1);
-	init_header_(/* ... */);
+	if (ftruncate(fd, 16 * 1024 * 1024) < 0)
+	{
+		close(fd);
+		return (2);
+	}
+	if (write(fd, HEAD, 12) <= 0)
+	{
+		close(fd);
+		return (3);
+	}
+	return (0);
 }
