1022a592db39752df64650b7912b55ceb528d2e7
Ian Wordes <iwordes@student.42.us.org>
2017-04-21 19:10:17 -0700

    State commit

--- src/db/init.c
+++ src/db/init.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 20:44:16 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/21 14:39:48 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/21 19:10:09 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	init_header_(int fd)
 
 	// 1. Write endian byte 0x00/0xff
 	buff[0] = EBYTE;
-	buff[1] = 0x42;
+	buff[1] = 0x2a;
 	*(uint16_t)(*&buff + 2) = 1;
 	write(fd, buff, 4);
 
