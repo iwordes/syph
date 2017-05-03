3ca5216acf07c9e4aa7521fc6c94b470c67feeed
Ian Wordes <iwordes@student.42.us.org>
2017-05-03 13:35:07 -0700

    Add tab_select

--- src/tab/select.c
+++ src/tab/select.c
@@ -6,27 +6,43 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 14:23:44 by iwordes           #+#    #+#             */
-/*   Updated: 2017/05/02 15:26:52 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/05/03 13:34:29 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <syph.h>
 
-/*
-** TODO
-** 1. Handle comparisons
-** 2. Find field location and size, write to socket
-*/
+#define C ts->req.cmp
+#define F ts->req.field
+
+#define CMP1 (tab_field(tab, C[i].id))
+#define CMP2 (ent + tab_foff(tab, C[i].id))
+
+#define CSIZE (tab_field(tab, C[i].id)->len * tab_field(tab, C[i].id)->size)
+
+#define SIZE (tab_field(tab, F[i])->len * tab_field(tab, F[i])->size)
 
 void	tab_select(t_tab *tab, U8 *ent, t_tabsel *ts)
 {
 	U8		i;
 
+	if (ts.i == ts.req.limit)
+		return ;
+
+	// Filtration
 	i = ~0;
+	val = ts->req.cmp_val;
 	while (++i < ts->req.cmp_len)
-		if (!ts->cmp[i](/* ... */))
+	{
+		if (!ts->cmp[i](CMP1, CMP2, val))
 			return ;
+		val += CSIZE;
+	}
+
+	// Selection
 	i = ~0;
-	while (++j < ts->req.field_len)
-		write(ts->sock, /* data */, /* len */);
+	while (++i < ts->req.field_len)
+		write(ts->sock, tab + tab_foff(tab, F[i]), SIZE);
+
+	ts.i += 1;
 }
