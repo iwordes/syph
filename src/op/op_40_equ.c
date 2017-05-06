8139570ca18f0fc21d7e89c8652cf8be1dbc8848
Ian Wordes <iwordes@student.42.us.org>
2017-05-05 20:13:58 -0700

    Potentially fix issue with 0x40

--- src/op/op_40_equ.c
+++ src/op/op_40_equ.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   40_equ.c                                           :+:      :+:    :+:   */
+/*   op_40_equ.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/30 15:48:10 by iwordes           #+#    #+#             */
-/*   Updated: 2017/05/04 14:50:25 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/05/05 20:13:44 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 bool	op_40_equ(t_field *meta, U8 *lhs, U8 *rhs)
 {
 	if (meta->type == SYT_UTF8)
-		return (strcmp((char*)lhs, (char*)rhs) == 0);
-	return (memcmp(lhs, rhs, meta->len * meta->size));
+		return (strncmp((char*)lhs, (char*)rhs, meta->len) == 0);
+	return (memcmp(lhs, rhs, meta->len * meta->size) == 0);
 }
