cd1ce3c7e43f7d017132a1ff803b464237d22423
Ian Wordes <iwordes@student.42.us.org>
2017-05-08 16:49:56 -0700

    Remove excess logging for tab_foff

--- src/tab/foff.c
+++ src/tab/foff.c
@@ -20,13 +20,11 @@ off_t	tab_foff(t_tab *tab, U8 id)
 	off_t	off;
 	U8		i;
 
-	LOG("\e[95mtab_foff\e[0m");
 	i = ~0;
 	off = 0;
 	if (tab->schema_len != 0)
 		id %= tab->schema_len;
 	while (++i < id)
 		off += SIZE;
-	LOG("\e[92mtab_foff\e[0m");
 	return (off);
 }
